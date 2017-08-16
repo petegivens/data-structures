2db656b02697864317884f937e8b16095fff30f7
Hack Reactor Student <student@hackreactor.com>
2017-08-16 15:22:39 -0500

    completed stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.length = 0;
+  return newStack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  'push' : function(value) {
+    if (typeof value === 'string') {
+      this[this.length] = value;
+      this.length++;
+    }
+  },
+  'pop' : function() {
+    var temp = this[this.length - 1];
+    delete this[this.length - 1];
+    if (this.length > 0) {
+      this.length--;
+    }
+    return temp;
+  },
+  'size' : function() {
+    return this.length;
+  }
+};
