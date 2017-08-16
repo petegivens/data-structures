6c77af08a9d7b913e1bffe63bbb8eef065447e99
Hack Reactor Student <student@hackreactor.com>
2017-08-16 16:22:06 -0500

    Added functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (typeof value === 'string') {
+      storage[size] = value;
+      size++;
+    }
   };
-
   someInstance.dequeue = function() {
+    var temp = storage[0];
+    delete storage[0];
+    if (size > 0) {
+      size--;
+    }
+    for (var i = 0; i < Object.keys(storage).length; i++ ) {
+      var temp2 = storage[i];
+      if(storage[i + 1]) {
+        storage[i] = storage[i + 1];
+      }
+      console.log('storage[' + i + ']:', storage[i])
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
