d6426894c6bc0562f771f12b506b3169c27269f1
simplenclean <ctbwilhite@gmail.com>
2017-08-17 14:11:11 -0500

    Added functionality for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,31 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var hasTarget = false;
+  // Base case
+  if ( this.value === target ) {
+    hasTarget = true;
+    return hasTarget;
+  }
+
+  // Recurse case
+  for (var i = 0; i < this.children.length; i++) {
+    hasTarget = hasTarget || this.children[i].contains(target);
+  }
+
+  return hasTarget;
 };
 
 
