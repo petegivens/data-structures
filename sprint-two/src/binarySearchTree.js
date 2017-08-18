984db6586731ce1b831858693b7a90160994e541
Hack Reactor Student <student@hackreactor.com>
2017-08-18 15:45:28 -0500

    completed minimum reqs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,45 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if ( value < this.value ) {
+    if ( this.left ) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else if ( value > this.value ) {
+    if ( this.right ) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
+};
 
+BinarySearchTree.prototype.contains = function(target) {
+  var containsTarget = false;
+
+  if ( this.value === target ) {
+    return true;
+  }
+  if ( target < this.value && this.left) {
+    containsTarget = this.left.contains(target)
+  } else if ( target > this.value && this.right) {
+    containsTarget = this.right.contains(target)
+  }
+  return containsTarget;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if ( this.left ) {
+    this.left.depthFirstLog(cb);
+  } if ( this.right ) {
+    this.right.depthFirstLog(cb)
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
